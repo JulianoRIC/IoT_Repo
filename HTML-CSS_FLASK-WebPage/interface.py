4ecc81e5c5006c522b7a36bac60d526279bb262b
G4 <jrs96.jr@gmail.com>
2022-06-10 14:47:57 -0300

    Interface gráfica - led broker

---
+++ HTML-CSS_FLASK-WebPage/interface.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, url_for, redirect
+import os
+import paho.mqtt.client as mqtt
+import time
+
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+
+
+#conectando com o broker MQTT
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect("192.168.1.106", 1883,60)
+client.username_pw_set("grupo4", "grupo4_22")
+
+app = Flask(__name__)
+
+
+@app.route("/home", methods=["GET", "POST"])
+
+def home():
+    if request.method == "POST":
+        print("Button was pressed ")
+
+        if request.form['btn_on'] == "Manual":
+            print("Modo Manual")
+            client.publish('iot/comandos', payload="manual", qos=0, retain=False)
+            return render_template("onoff.html")
+            
+
+        if request.form['btn_on'] == "Automático":
+            print("Modo Automático")
+            client.publish('iot/comandos', payload="auto", qos=0, retain=False) 
+            return render_template("onoff.html")
+
+
+    return render_template("onoff.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#client.loop_forever()
