27a8f3cd45edf6627e436e47d0aff4a190434788
Cristian Gislon <cristian.debiasigislon@gmail.com>
2022-07-14 13:45:56 -0300

    Atualização interface

--- source-interface/interface.py
+++ source-interface/interface.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, flash, render_template, request, url_for, redirect, flash
 import os
 import paho.mqtt.client as mqtt
 import time
 import webbrowser
 
-
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
+
+app = Flask(__name__, static_url_path='/static')
+app.static_folder = 'static'
+app.config['SESSION_COOKIE_SECURE'] = False
 
 
 #conectando com o broker MQTT
@@ -15,12 +18,10 @@ client.on_connect = on_connect
 client.connect("192.168.1.106", 1883,60)
 client.username_pw_set("grupo4", "grupo4_22")
 
-app = Flask(__name__)
-
-
 @app.route("/home", methods=["GET", "POST"])
 
 def home():
+<<<<<<< Updated upstream
     if request.method == "POST":
         print("Button was pressed ")
 
@@ -35,18 +36,66 @@ def home():
             print("Modo Manual")
             client.publish('iot/comandos', payload="manual", qos=0, retain=False)
             return render_template("index.html")
+=======
+
+    return render_template("indexLogin.html")
+    
+@app.route("/login", methods=["GET", "POST"])
+
+def login():
+    if request.form.get('usuario') == "admin" and request.form.get('senha') == "admin" :
+        return render_template("index.html")
+    else:
+        return render_template("indexLogin.html")
+
+
+
+@app.route("/man", methods=["GET", "POST"])
+
+def man():
+    
+    client.on_connect = on_connect
+    client.connect("192.168.1.106", 1883,60)
+    client.username_pw_set("grupo4", "grupo4_22")
+    temp = request.form.get('TEMP')
+    print(temp)
+    #client.publish('iot/comandos', payload= temp, qos=0, retain=False)
+    return render_template("index.html", temp = temp)
+>>>>>>> Stashed changes
             
 
-        if request.form['btn_on'] == "Automático":
-            print("Modo Automático")
-            client.on_connect = on_connect
-            client.connect("192.168.1.106", 1883,60)
-            client.username_pw_set("grupo4", "grupo4_22")
-            client.publish('iot/comandos', payload="auto", qos=0, retain=False) 
-            return render_template("index.html")
 
+@app.route("/auto", methods=["GET", "POST"])
 
+def auto():    
+    potencia = request.form.get('POT')
+    print("Modo Automático")
+    print(potencia)
+    #client.on_connect = on_connect
     
+    client.connect("192.168.1.106", 1883,60)
+    client.username_pw_set("grupo4", "grupo4_22")
+    client.publish('iot/comandos', payload= potencia, qos=0, retain=False) 
+    return render_template("index.html", pot = potencia)
+            
+@app.route("/on", methods=["GET", "POST"])
+
+def on():    
+    print("Liga enviado")
+    client.on_connect = on_connect    
+    client.connect("192.168.1.106", 1883,60)
+    client.username_pw_set("grupo4", "grupo4_22")
+    client.publish('iot/comandos', payload= "on", qos=0, retain=False) 
+    return render_template("index.html")
+
+@app.route("/off", methods=["GET", "POST"])
+
+def off():    
+    print("Desliga enviado")
+    client.on_connect = on_connect    
+    client.connect("192.168.1.106", 1883,60)
+    client.username_pw_set("grupo4", "grupo4_22")
+    client.publish('iot/comandos', payload= "off", qos=0, retain=False) 
     return render_template("index.html")
 
     
